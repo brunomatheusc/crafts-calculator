52cdd71af0fc9d84d1a7471b15022839444a2b6a
Bruno Matheus <bmatheusc@gmail.com>
2021-02-18 21:41:24 -0300

    Initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import { Container } from './styles';
+
+const Home: FC = () => {
+    return (
+        <Container>
+            <h2>Home</h2>
+        </Container>
+    );
+}
+
+export default Home;
